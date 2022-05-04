f3a219c6a50ed5d0d893c474032f6e9563bee313
Noah Mattocks <noahmattocks@gmail.com>
2022-05-04 10:17:14 -0700

    Added index.js to models

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+const Client = require('./Client');
+const Employee = require('./Employee');
+const Manager = require('./Manager');
+const Project = require('./Project');
+
+module.exports = { Client, Employee, Manager, Project };
