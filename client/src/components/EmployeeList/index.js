38c11d8ca12d266ecd7e3107e2040406559aaa61
EthanP94 <ethan.plott@yahoo.com>
2022-05-06 13:58:25 -0700

    images for expertice added

--- client/src/components/EmployeeList/index.js
+++ client/src/components/EmployeeList/index.js
@@ -5,10 +5,21 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import image from '../../images/project-cards.jpg'
 import { Link } from 'react-router-dom';
+import Landscaping from "../../images/work/Landscaping.jpg"
+import Carpentry from "../../images/work/Carpentry.jpg"
 
 const BasicCard = ({
     id = 0, openModal, employees
 }) => {
+    const renderImage = (type) => {
+        if (type === "Landscaping") {
+            return Landscaping
+        } else if (type === "Carpentry") {
+            return Carpentry
+        } else {
+            return image
+        }
+    }
     return (
         employees.map(employee => (
             <div>
@@ -17,7 +28,7 @@ const BasicCard = ({
                         <CardContent>
                         <CardMedia
                         component="img"
-                        image= {image}
+                        image= {renderImage (employee.expertise)}
                         alt="home remodeling tools"
                         />
                         <Typography variant="h5" component="div" style={{ marginTop: '20px' }}>
